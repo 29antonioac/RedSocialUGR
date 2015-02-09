a03b7b835924170959b8cc440e1d7ce9837d1350
analca3 <analca3@gmail.com>
2015-02-09 20:07:09 +0100

    Añadidas tablas de Muro/Post

--- RedSocialUGR.sql
+++ RedSocialUGR.sql
@@ -14,7 +14,7 @@
 
  CREATE TABLE estudiantes (
    Correo           VARCHAR(50) NOT NULL,
-   Nombre           VARCHAR(50) NOT NULL,
+   NombreEstudiante VARCHAR(50) NOT NULL,
    Apellidos        VARCHAR(50) NOT NULL,
    FechaNacimiento  DATE        NOT NULL,
    FotoPersonal     VARCHAR(70) DEFAULT NULL,
@@ -22,22 +22,21 @@
    Hobbies          VARCHAR(50) DEFAULT NULL,
    EstadoActual     VARCHAR(30) DEFAULT NULL,
    Ocupacion        VARCHAR(30) DEFAULT NULL,
-   PRIMARY KEY(correo)
+   PRIMARY KEY  (correo)
  );
 
  CREATE TABLE carreras (
    Nombre         VARCHAR(30) NOT NULL,
    Centro         VARCHAR(30) NOT NULL,
-   PRIMARY KEY(nombre)
+   PRIMARY KEY  (nombre)
  );
 
  CREATE TABLE cursa (
-   Correo_alumno  VARCHAR(50) NOT NULL,
-   Nombre_carrera VARCHAR(50) NOT NULL,
-   KEY            (correo_alumno),
-   KEY            (nombre_carrera),
-   FOREIGN KEY    (correo_alumno)   REFERENCES estudiantes  (correo)  ON DELETE CASCADE,
-   FOREIGN KEY    (nombre_carrera)  REFERENCES carreras     (nombre)  ON DELETE CASCADE
+   CorreoAlumno  VARCHAR(50) NOT NULL,
+   NombreCarrera VARCHAR(50) NOT NULL,
+   PRIMARY KEY    (CorreoAlumno, NombreCarrera),
+   FOREIGN KEY    (CorreoAlumno)   REFERENCES estudiantes  (Correo) ON DELETE CASCADE,
+   FOREIGN KEY    (NombreCarrera)  REFERENCES carreras     (Nombre) ON DELETE CASCADE
  );
 
 
@@ -49,20 +48,93 @@
 
  -- Creación de la tabla muros
 
+ CREATE TABLE foro (
+   IdentificadorForo  VARCHAR(50) NOT NULL,
+   NombreForo         VARCHAR(50) NOT NULL,
+   PRIMARY KEY  (IdentificadorForo)
+ );
+
+ CREATE TABLE tema (
+   IdentificadorForo  VARCHAR(50) NOT NULL,
+   IdentificadorTema  VARCHAR(50) NOT NULL,
+   NombreTema         VARCHAR(50) NOT NULL,
+   PRIMARY KEY  (IdentificadorForo, IdentificadorTema),
+   FOREIGN KEY  (IdentificadorForo)   REFERENCES foro  (IdentificadorForo) ON DELETE CASCADE
+ );
+
+ CREATE TABLE post (
+   IdentificadorForo  VARCHAR(50) NOT NULL,
+   IdentificadorTema  VARCHAR(50) NOT NULL,
+   IdentificadorPost  VARCHAR(50) NOT NULL,
+   CotenidoPost       VARCHAR(200) NOT NULL,
+   PRIMARY KEY    (IdentificadorForo, IdentificadorTema, IdentificadorPost),
+   FOREIGN KEY    (IdentificadorForo, IdentificadorTema)   REFERENCES tema  (IdentificadorForo,IdentificadorTema) ON DELETE CASCADE
+
+ );
+
+ CREATE TABLE muro (
+   IdentificadorMuro VARCHAR(50) NOT NULL,
+   PRIMARY KEY  (IdentificadorMuro)
+ );
+
+ CREATE TABLE mensaje (
+   IdentificadorMuro    VARCHAR(50) NOT NULL,
+   IdentificadorMensaje VARCHAR(50) NOT NULL,
+   CotenidoMensaje      VARCHAR(50) NOT NULL,
+   PRIMARY KEY    (IdentificadorMuro, IdentificadorMensaje),
+   FOREIGN KEY    (IdentificadorMuro)   REFERENCES muro  (IdentificadorMuro) ON DELETE CASCADE
+ );
+
+ CREATE TABLE crea (
+   IdentificadorForo  VARCHAR(50) NOT NULL,
+   IdentificadorTema  VARCHAR(50) NOT NULL,
+   ContenidoMensaje   VARCHAR(200) NOT NULL,
+   Correo             VARCHAR(50) NOT NULL,
+   FechaCrea          DATE NOT NULL,
+   PRIMARY KEY    (IdentificadorTema),
+   FOREIGN KEY    (IdentificadorForo, IdentificadorTema)   REFERENCES tema  (IdentificadorForo,IdentificadorTema) ON DELETE CASCADE,
+   FOREIGN KEY    (Correo)   REFERENCES estudiantes   (Correo) ON DELETE CASCADE
+ );
+
+  CREATE TABLE escribe1 (
+    IdentificadorForo   VARCHAR(50) NOT NULL,
+    IdentificadorTema   VARCHAR(50) NOT NULL,
+    IdentificadorPost   VARCHAR(50) NOT NULL,
+    Correo              VARCHAR(50) NOT NULL,
+    FechaEscribe1       DATE NOT NULL,
+    PRIMARY KEY     (IdentificadorForo, IdentificadorTema, IdentificadorPost),
+    FOREIGN KEY     (IdentificadorForo, IdentificadorTema, IdentificadorPost)   REFERENCES post  (IdentificadorForo,IdentificadorTema, IdentificadorPost) ON DELETE CASCADE,
+    FOREIGN KEY     (Correo)   REFERENCES estudiantes   (Correo) ON DELETE CASCADE
+  );
+
+  CREATE TABLE escribe2 (
+    IdentificadorMuro     VARCHAR(50) NOT NULL,
+    IdentificadorMensaje  VARCHAR(50) NOT NULL,
+    Correo                VARCHAR(50) NOT NULL,
+    FechaEscribe2         DATE NOT NULL,
+    PRIMARY KEY     (IdentificadorMuro, IdentificadorMensaje),
+    FOREIGN KEY     (IdentificadorMuro, IdentificadorMensaje)   REFERENCES mensaje  (IdentificadorMuro,IdentificadorMensaje) ON DELETE CASCADE,
+    FOREIGN KEY     (Correo)   REFERENCES estudiantes   (Correo) ON DELETE CASCADE
+  );
+
+ -- Disparadores
+
+ -- Eva
+ -- SELECT max(IdentificadorForo) FROM foro
+ -- Antonio
+
+ -- Carlos
+
+ -- Adrián
 
  -- NOTA: Lo suyo es echar esto a otros archivos y traerlos con "source <file>;"
  -- Inserción de tuplas en estudiantes
 
- -- correo           VARCHAR(50) NOT NULL,
- -- nombre           VARCHAR(50) NOT NULL,
- -- apellidos        VARCHAR(50) NOT NULL,
- -- fechaNacimiento  DATE        NOT NULL,
-
- INSERT INTO estudiantes (correo,nombre,apellidos,fechaNacimiento) VALUES
+ INSERT INTO estudiantes (Correo,NombreEstudiante,Apellidos,FechaNacimiento) VALUES
  ('antonio@correo.ugr.es','Antonio','Álvarez Caballero',"1993-11-29"),
- ('evamaria@correo.ugr.es','Eva María','González García',"1992-02-01"),
- ('carlos@correo.ugr.es','Carlos','Paradela Pérez',"1992-02-02"),
- ('adrian@correo.ugr.es','Adrián','Ranea Robles',"1993-02-02");
+ ('evamaria@correo.ugr.es','Eva María','González García',"1992-09-01"),
+ ('carlos@correo.ugr.es','Carlos','Parla Pérez',"1992-02-02"),
+ ('adrian@correo.ugr.es','Adrián','Rho Rha',"1993-02-02");
 
  -- Inserción de tuplas en carreras
 
@@ -70,14 +142,14 @@
  -- centro         VARCHAR(30) NOT NULL,
  -- PRIMARY KEY(nombre)
 
- INSERT INTO carreras (nombre, centro) VALUES
+ INSERT INTO carreras (Nombre, Centro) VALUES
  ('Doble Grado', 'ETSIIT/Ciencias');
 
  -- Inserción de tuplas en cursa
  -- correo_alumno  VARCHAR(50) NOT NULL,
  -- nombre_carrera VARCHAR(50) NOT NULL,
 
- INSERT INTO cursa (correo_alumno, nombre_carrera) VALUES
+ INSERT INTO cursa (CorreoAlumno, NombreCarrera) VALUES
  ('antonio@correo.ugr.es','Doble Grado'),
  ('evamaria@correo.ugr.es','Doble Grado'),
  ('carlos@correo.ugr.es','Doble Grado'),
